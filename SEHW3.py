d960b2307c84636f6802d6a89af7db718ebba8bf
Emma Stubby <emma.stubby@gmail.com>
2023-09-11 13:17:58 -0500

    add sum_list method

---
+++ SEHW3.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep 11 13:09:25 2023
+
+@author: emmastubby
+"""
+
+# method to sum list
+def sum_list(my_list):
+    my_sum = 0
+    for num in my_list:
+        my_sum += num
+    return my_sum
+
